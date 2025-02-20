15ea648e076a3d840f6ba621242c1bfe2af998cc
Oz996 <ozgun996@hotmail.com>
2025-02-20 17:16:14 +0100

    initialized prisma and connection to db

--- app/(store)/(main)/home/page.tsx
+++ app/(store)/(main)/home/page.tsx
@@ -1,6 +1,9 @@
+import prisma from "@/lib/prisma";
 import styles from "./page.module.scss";
 
-export default function Home() {
+export default async function Home() {
+  const prod = await prisma.product.findMany();
+  console.log("prodd", prod);
   return (
     <section className={styles.section}>
       <div></div>
