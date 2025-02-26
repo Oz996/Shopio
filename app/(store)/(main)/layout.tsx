0bc3bff4be74001c7c700e6a3ade1bd6ba45872e
Oz996 <ozgun996@hotmail.com>
2025-02-26 10:15:59 +0100

    improvements and changes to projects layout, added dynamic mixin for line clamps

--- app/(store)/(main)/layout.tsx
+++ app/(store)/(main)/layout.tsx
@@ -1,12 +1,12 @@
 import Header from "@/components/header/header";
-import styles from "./home/page.module.scss";
+import styles from "./layout.module.scss";
 import { ReactNode } from "react";
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header />
-      <main className={styles.layout}>{children}</main>
+      <main className={styles.store_wrapper}>{children}</main>
     </>
   );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -8,27 +8,29 @@ export default function Header() {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.logo}>
-          <Link href="/home">
-            <h2>Shopio</h2>
-          </Link>
+        <div className={styles.header_wrapper}>
+          <div className={styles.logo}>
+            <Link href="/home">
+              <h2>Shopio</h2>
+            </Link>
+          </div>
+          <nav className={styles.nav}>
+            <div className={styles.input_div}>
+              <input
+                type="text"
+                placeholder="Search for products"
+                className={styles.input}
+              />
+              <Search className={styles.icon_search} size={20} />
+            </div>
+
+            <ProductsNavbar />
+
+            <div className={styles.nav_item}>
+              <span className={styles.text}>Sign Up / Login</span>
+            </div>
+          </nav>
         </div>
-        <nav className={styles.nav}>
-          <div className={styles.input_div}>
-            <input
-              type="text"
-              placeholder="Search for products"
-              className={styles.input}
-            />
-            <Search className={styles.icon_search} size={20} />
-          </div>
-
-          <ProductsNavbar />
-
-          <div className={styles.nav_item}>
-            <span className={styles.text}>Sign Up / Login</span>
-          </div>
-        </nav>
       </header>
 
       <HeaderMobile />
