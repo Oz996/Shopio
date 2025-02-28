4d1db3be243cfeae41d6ac9c3d4315586e4c6c44
Oz996 <ozgun996@hotmail.com>
2025-03-01 00:16:04 +0100

    product details page and interactivity

--- app/(store)/(main)/product/[slug]/components/product-details/product-details.tsx
+++ app/(store)/(main)/product/[slug]/components/product-details/product-details.tsx
@@ -1,14 +1,45 @@
 import { Product } from "@prisma/client";
 import styles from "./product-details.module.scss";
+import ProductOptions from "../product-options/product-options";
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
 
 interface ProductDetailsProps {
   product: Product;
 }
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
+  function listDescription(description: string) {
+    return (
+      <ul className={styles.list}>
+        {description.split("-").map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  console.log("test1", listDescription(product.description));
+
   return (
     <div className={styles.details}>
-      <div></div>
+      <h1>{`${product.brand} ${product.name}`}</h1>
+      <p className={styles.details_text}>{product.details}</p>
+      {listDescription(product.description)}
+      <span className={styles.price}>€ {product.price}</span>
+
+      <ProductOptions product={product} />
+
+      <div className={styles.links}>
+        <Link href={`/products/${product.brand}`}>
+          Browse more from <span>{product.brand}</span>
+          <ChevronRight size={15} />
+        </Link>
+        <Link href={`/products/${product.category}`}>
+          {`Browse all ${product.category}`}
+          <ChevronRight size={15} />
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ app/(store)/(main)/product/[slug]/components/product-options/product-options.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Product } from "@prisma/client";
+import styles from "./product-option.module.scss";
+import { ChangeEvent, useState } from "react";
+import { ShoppingBasket } from "lucide-react";
+
+interface ProductOptionsProps {
+  product: Product;
+}
+
+export default function ProductOptions({ product }: ProductOptionsProps) {
+  const [amount, setAmount] = useState(1);
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setAmount(Number(e.target.value));
+  }
+
+  function incrementAmount() {
+    setAmount((prev) => prev + 1);
+  }
+
+  function decrementAmount() {
+    if (amount > 0) {
+      setAmount((prev) => prev - 1);
+    }
+  }
+
+  return (
+    <div className={styles.options}>
+      <div className={styles.amount}>
+        <button onClick={decrementAmount}>-</button>
+        <input type="text" value={amount} onChange={handleChange} />
+        <button onClick={incrementAmount}>+</button>
+      </div>
+      <button className={styles.add_button}>
+        Add to Cart <ShoppingBasket size={16} />{" "}
+      </button>
+    </div>
+  );
+}
