2ef6f2f0798efa7e9fda775156dfdc4e5cd1e863
Oz996 <ozgun996@hotmail.com>
2025-05-02 13:23:22 +0200

    using react-select for review sorting

--- app/(store)/(main)/product/[slug]/components/product-reviews/product-review-sheet/review-sort/review-sort.tsx
+++ app/(store)/(main)/product/[slug]/components/product-reviews/product-review-sheet/review-sort/review-sort.tsx
@@ -1,31 +1,63 @@
-import { Review } from "@prisma/client";
-import styles from "./review-sort.module.scss";
-import { Dispatch, SetStateAction } from "react";
+"use client";
+
+import dynamic from "next/dynamic";
 import { ProductSortType } from "../product-review-sheet";
+import styles from "./review-sort.module.scss";
+const ReactSelect = dynamic(() => import("react-select"), { ssr: false });
 
 interface ReviewSortProps {
   currentSort: ProductSortType;
   sortReviews: (sort: ProductSortType) => void;
 }
 
+type SortName = "Latest" | `${Capitalize<ProductSortType>} rating`;
+
+interface SortOptions {
+  value: ProductSortType;
+  label: SortName;
+}
+
+const options: SortOptions[] = [
+  { value: "latest", label: "Latest" },
+  { value: "high", label: "High rating" },
+  { value: "low", label: "Low rating" },
+];
+
 export default function ReviewSort({
   currentSort,
   sortReviews,
 }: ReviewSortProps) {
-  const sortOptions: ProductSortType[] = ["latest", "high", "low"];
+  const selectedSort = options.find((option) => option.value === currentSort);
 
   return (
     <div className={styles.sort}>
-      <select
-        name="sort"
-        value={currentSort}
-        aria-label="sort reviews"
-        onChange={(e) => sortReviews(e.target.value as ProductSortType)}
-      >
-        {sortOptions.map((option) => (
-          <option key={option}>{option}</option>
-        ))}
-      </select>
+      <ReactSelect
+        value={selectedSort}
+        styles={customStyles}
+        options={options}
+        onChange={(selected) => sortReviews((selected as any).value)}
+        aria-label="Sort reviews"
+        isSearchable={false}
+      />
     </div>
   );
 }
+
+const customStyles = {
+  control: (provided: any) => ({
+    ...provided,
+    fontSize: "14px",
+    cursor: "pointer",
+  }),
+  option: (provided: any, state: any) => ({
+    ...provided,
+    fontSize: "14px",
+    backgroundColor: state.isFocused ? "#e4e9f4" : "white",
+    cursor: "pointer",
+    color: "black",
+  }),
+  singleValue: (provided: any) => ({
+    ...provided,
+    fontSize: "14px",
+  }),
+};
